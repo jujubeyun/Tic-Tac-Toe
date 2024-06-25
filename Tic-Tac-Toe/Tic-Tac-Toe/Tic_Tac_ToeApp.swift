716c3c36692dff8ab958d39f50662f82985e9cbe
normalturtle <kre0708@naver.com>
2024-06-25 09:34:17 +0900

    create Tic-Tac-Toe

---
+++ Tic-Tac-Toe/Tic-Tac-Toe/Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_ToeApp.swift
+//  Tic-Tac-Toe
+//
+//  Created by Juhyun Yun on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
