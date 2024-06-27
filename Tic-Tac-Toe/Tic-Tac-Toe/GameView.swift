83f5cac97d9eeb982bdd51213fa2acd29d079982
normalturtle <kre0708@naver.com>
2024-06-27 16:24:13 +0900

    feat: add GameView

---
+++ Tic-Tac-Toe/Tic-Tac-Toe/GameView.swift
@@ -0,0 +1,42 @@
+//
+//  GameView.swift
+//  Tic-Tac-Toe
+//
+//  Created by Juhyun Yun on 6/27/24.
+//
+
+import SwiftUI
+
+struct GameView: View {
+    
+    let columns = Array(repeating: GridItem(.flexible()), count: 3)
+    
+    @State var moves: [String?] = Array(repeating: nil, count: 9)
+    
+    var body: some View {
+        VStack {
+            LazyVGrid(columns: columns) {
+                ForEach(0..<9) { i in
+                    ZStack {
+                        Color(.secondarySystemBackground)
+                            .aspectRatio(contentMode: .fit)
+                        
+                        Image(systemName: moves[i] ?? "")
+                            .resizable()
+                            .fontWeight(.medium)
+                            .padding(24)
+                    }
+                    .onTapGesture {
+                        let isEven = i % 2 == 0
+                        moves[i] = isEven ? "xmark" : "circle"
+                    }
+                }
+            }
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    GameView()
+}
