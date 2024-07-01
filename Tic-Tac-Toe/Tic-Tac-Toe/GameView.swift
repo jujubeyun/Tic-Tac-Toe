ad4dde83603874673500529170fa144e7f320f5d
normalturtle <kre0708@naver.com>
2024-07-01 17:06:34 +0900

    feat: add ScoreView

--- Tic-Tac-Toe/Tic-Tac-Toe/GameView.swift
+++ Tic-Tac-Toe/Tic-Tac-Toe/GameView.swift
@@ -15,13 +15,19 @@ struct GameView: View {
     
     var body: some View {
         VStack {
+            
+            HStack(spacing: 30) {
+                ScoreView(symbol: "xmark", score: 0)
+                ScoreView(symbol: "circle", score: 0)
+            }
+            
             LazyVGrid(columns: columns) {
                 ForEach(0..<9) { i in
                     ZStack {
                         RoundedRectangle(cornerRadius: 10)
                             .aspectRatio(contentMode: .fit)
                             .foregroundStyle(.indigo.gradient)
-                            .opacity(0.3)
+                            .opacity(0.5)
                         
                         Image(systemName: moves[i] ?? "")
                             .resizable()
@@ -41,4 +47,27 @@ struct GameView: View {
 
 #Preview {
     GameView()
+}
+
+struct ScoreView: View {
+    
+    var symbol: String
+    var score: Int
+    
+    var body: some View {
+        HStack(spacing: 30) {
+            Image(systemName: symbol)
+                .font(.title)
+                .bold()
+            
+            
+            Text(score == 0 ? "-" : "\(score)")
+                .font(.title2)
+        }
+        .frame(width: 150, height: 50)
+        .overlay {
+            RoundedRectangle(cornerRadius: 10)
+                .stroke(Color(.label), lineWidth: 3)
+        }
+    }
 }
