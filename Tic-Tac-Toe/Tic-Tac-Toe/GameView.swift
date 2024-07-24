42ed09e4b41c8a28eafe548bc9abd97b5d988818
jujubeyun <kre0708@naver.com>
2024-07-24 18:00:57 +0900

    implement singleplay

--- Tic-Tac-Toe/Tic-Tac-Toe/GameView.swift
+++ Tic-Tac-Toe/Tic-Tac-Toe/GameView.swift
@@ -19,6 +19,7 @@ struct GameView: View {
     @State var alertItem: AlertItem?
     @State var player1Score = 0
     @State var player2Score = 0
+    @State var isGameBoardDisabled = false
     
     let winPatterns: Set<Set<Int>> = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
     
@@ -33,18 +34,15 @@ struct GameView: View {
             
             LazyVGrid(columns: Array(repeating: GridItem(.flexible()), count: 3)) {
                 ForEach(0..<9) { i in
-                    let player: Player = moves.compactMap { $0 }.count % 2 == 0 ? .player1 : .player2
                     ZStack {
                         GameSquareView(move: moves[i])
                             .onTapGesture {
-                                guard moves[i] == nil else { return }
-                                moves[i] = Move(player: player, boardIndex: i)
-                                checkForDraw()
-                                checkWinCondition(for: player)
+                                processPlayerMove(for: i)
                             }
                     }
                 }
             }
+            .disabled(isGameBoardDisabled)
             .padding()
             .alert(item: $alertItem) { alertItem in
                 Alert(title: alertItem.title, message: alertItem.message, dismissButton: .default(alertItem.buttonTitle, action: { resetGame() }))
@@ -52,26 +50,107 @@ struct GameView: View {
         }
     }
     
-    func checkWinCondition(for player: Player) {
-        let playerMoves = moves.compactMap { $0 }.filter { $0.player == player }
-        let playerPositions = Set(playerMoves.map { $0.boardIndex })
+    func processPlayerMove(for position: Int) {
+        let player: Player = moves.compactMap { $0 }.count % 2 == 0 ? .player1 : .player2
+        if isSquareOccupied(forIndex: position) { return }
+        moves[position] = Move(player: player, boardIndex: position)
         
-        for pattern in winPatterns where pattern.isSubset(of: playerPositions) { 
-            switch player {
-            case .player1:
-                alertItem = AlertContext.player1Win
-                player1Score += 1
-            case .player2:
-                alertItem = AlertContext.player2Win
-                player2Score += 1
+        if checkWinCondition(for: player) {
+            alertItem = AlertContext.player1Win
+            player1Score += player == .player1 ? 1 : 0
+            player2Score += player == .player2 ? 1 : 0
+            return
+        }
+        
+        if checkForDraw() {
+            alertItem = AlertContext.draw
+            return
+        }
+        
+        isGameBoardDisabled = true
+        
+        if gameSetting.isSinglePlayer {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) { [self] in
+                isGameBoardDisabled = false
+                let computerPosition = determineComputerMovePosition()
+                moves[computerPosition] = Move(player: .player2, boardIndex: computerPosition)
+                
+                if checkWinCondition(for: .player2) {
+                    alertItem = AlertContext.player2Win
+                    player2Score += 1
+                    return
+                }
+                
+                if checkForDraw() {
+                    alertItem = AlertContext.draw
+                    return
+                }
             }
+        } else {
+            isGameBoardDisabled = false
         }
     }
     
-    func checkForDraw() {
-        if moves.compactMap({$0}).count == 9 {
-            alertItem = AlertContext.draw
+    func determineComputerMovePosition() -> Int {
+        // If AI can win, then win
+        let computerMoves = moves.compactMap { $0 }.filter { $0.player == .player2 }
+        let computerPositions = Set(computerMoves.map { $0.boardIndex })
+        
+        for pattern in winPatterns {
+            let winPositions = pattern.subtracting(computerPositions)
+            
+            if winPositions.count == 1 {
+                let isAvailable = !isSquareOccupied(forIndex: winPositions.first!)
+                if isAvailable { return winPositions.first! }
+            }
         }
+        
+//         If AI can't win, then block
+        let humanMoves = moves.compactMap { $0 }.filter { $0.player == .player1 }
+        let humanPositions = Set(humanMoves.map { $0.boardIndex })
+        
+        for pattern in winPatterns {
+            let winPositions = pattern.subtracting(humanPositions)
+            
+            if winPositions.count == 1 {
+                let isAvailable = !isSquareOccupied(forIndex: winPositions.first!)
+                if isAvailable { return winPositions.first! }
+            }
+        }
+        
+//         If AI can't block, then take middle square
+        let centerSquare = 4
+        if !isSquareOccupied(forIndex: centerSquare) {
+            return centerSquare
+        }
+        
+        
+        // If AI can't take middle square, take random available square
+        var movePosition = Int.random(in: 0..<9)
+        
+        while isSquareOccupied(forIndex: movePosition) {
+            movePosition = Int.random(in: 0..<9)
+        }
+        
+        return movePosition
+    }
+    
+    func isSquareOccupied(forIndex index: Int) -> Bool {
+        return moves.contains { $0?.boardIndex == index }
+    }
+    
+    func checkWinCondition(for player: Player) -> Bool {
+        let winPatterns: Set<Set<Int>> = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+        
+        let playerMoves = moves.compactMap { $0 }.filter { $0.player == player }
+        let playerPositions = Set(playerMoves.map { $0.boardIndex })
+        
+        for pattern in winPatterns where pattern.isSubset(of: playerPositions) { return true }
+        return false
+    }
+    
+    func checkForDraw() -> Bool {
+        return moves.compactMap { $0 }.count == 9
     }
     
     func resetGame() {
